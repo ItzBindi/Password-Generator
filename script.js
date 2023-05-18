6c6a0a002bcda9774e4a073e9ad8960195a55171
Suraj <snair306@gmail.com>
2023-05-18 15:41:21 -0500

    finished

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ function writePassword() {
 
 var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
+var numberOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialOptions = ["!", "@", "#", "$","%","&"]
+var useroptions = []
 
 function generatePassword() {
   var passwordLength = prompt("Pick a length for your password bewteen 8 to 128 characters.")
@@ -27,32 +28,46 @@ function generatePassword() {
 
 var lowerCase = confirm("Do you want lowercase characters in your password?")
 var upperCase = confirm("Do you want uppercase characters in your password?")
-
-if (lowerCase !== true && upperCase !== true) {
+var number = confirm("Do you want numbers in your password?")
+var special = confirm("Do you want special characters in your password?")
+if (lowerCase !== true && upperCase !== true && number !== true && special !== true ) {
   alert("Make sure to select atleast one character type!")
   generatePassword();
-}else{
-  var letters = lowerCaseOptions.concat(upperCaseOptions);
-  var randNum = Math.floor(Math.random() *letters.length);
-  console.log(randNum);
-  var pickletter = letters[randNum]
-  console.log("machine picks" + " " + pickletter);
-
+}else {
+  if (lowerCase) {
+useroptions = useroptions.concat(lowerCaseOptions);
 }
+  if (upperCase) {
+  useroptions = useroptions.concat(upperCaseOptions);
+  }
+  if (number) {
+    useroptions = useroptions.concat(numberOptions);
+  }
+  if (special) {
+    useroptions = useroptions.concat(specialOptions);
+  }
+}
+console.log(useroptions);
+ 
+
+var newPassword = ""
 
 for(let i = 0; i < passwordLength; i++) {
-  passwordLength[i] + generateBtn;
-
-
+  // passwordLength[i] + generateBtn;
+  var randLet = Math.floor(Math.random() *useroptions.length);
+  console.log(randLet);
+  var pickletter = useroptions[randLet]
+  console.log("machine picks" + " " + pickletter);
+  newPassword += pickletter
 
 
 
 
 }
-
+console.log(newPassword);
+return newPassword;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-generatePassword();
